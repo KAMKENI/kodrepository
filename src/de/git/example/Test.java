3f42fa452eb1a9cd0bae6f6f7c4d3069946d7bcd
KAMKENI <kodssi@hotmail.de>
2013-04-29 12:47:47 +0200

    mytest 10

--- src/de/git/example/Test.java
+++ src/de/git/example/Test.java
@@ -2,6 +2,6 @@ package de.git.example;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello mein test 7");
+		System.out.println("Hello mein test 10");
 	}
 }
