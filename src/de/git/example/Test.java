05a4b63bb7d9ffaaca82467025af40a72edc8155
KAMKENI <KODSSI@HOTMAIL.DE>
2013-04-29 16:18:44 +0300

    remote 33

--- src/de/git/example/Test.java
+++ src/de/git/example/Test.java
@@ -3,7 +3,7 @@ package de.git.example;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Hello mein test 32");
+		System.out.println("Hello mein test 33");
 
 }
 }
