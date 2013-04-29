654bee6f0dae3f8bf55559c1a4a711c8e07f94bb
KAMKENI <kodssi@hotmail.de>
2013-04-29 16:58:25 +0200

    rwerwer

--- src/de/git/example/Test.java
+++ src/de/git/example/Test.java
@@ -3,6 +3,6 @@ package de.git.example;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Hello mein test 49");
+		System.out.println("Hello mein test 52");
 }
 }
